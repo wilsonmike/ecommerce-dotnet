01b9240c8b1bacd15a5f4a4b05a0cf73a1d3db7e
mike <michaelwilsonsj@hotmail.com>
2020-11-26 22:07:41 -0500

    api working / dummy data

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,4 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using skinet.API.Data;
+using skinet.API.Entities;
 
 namespace skinet.API.Controllers
 {
@@ -6,15 +12,22 @@ namespace skinet.API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
-        [HttpGet]
-        public string GetProducts()
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
         {
-            return "this will be a list of products";
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
         }
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return "single product";
+            return await _context.Products.FindAsync(id); 
         }
     }
 }
